c2f2bcb8160e437bfaa2a7be81cdbdb32e73eda1
Jonathan Tirapegui <137382571+damiantirapegui@users.noreply.github.com>
2024-05-10 22:14:43 +0200

    Uppdated, almost ready

--- src/Pages/Github/Github.jsx
+++ src/Pages/Github/Github.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faCss3Alt, faReact } from "@fortawesome/free-brands-svg-icons";
+import { faJs } from "@fortawesome/free-brands-svg-icons";
+import { faHtml5 } from "@fortawesome/free-brands-svg-icons";
+
 import {
   fetchReposFailure,
   fetchReposRequest,
   fetchReposSuccess,
 } from "../../redux/action";
-// import {
-//   fetchReposFailure,
-//   fetchReposRequest,
-//   fetchReposSuccess,
-// }
-//  from "../../redux/actions";
+
 import "./Github.css";
 
 function Github() {
@@ -19,7 +20,7 @@ function Github() {
   const repos = useSelector((state) => state.repos);
   const error = useSelector((state) => state.error);
   const accessKey = import.meta.env.VITE_ACCESS_KEY;
-  console.log(accessKey);
+
   useEffect(() => {
     async function fetchRepos() {
       dispatch(fetchReposRequest());
@@ -33,11 +34,13 @@ function Github() {
       } catch (error) {
         dispatch(fetchReposFailure(error.message));
       }
-      console.log("hej");
-      console.log(accessKey);
     }
     fetchRepos();
   }, [dispatch]);
+
+  const filteredRepos = repos.filter((repo) =>
+    ["To-do-list", "Solaris_FE23-Jonathan_Tirapegui"].includes(repo.name)
+  );
 
   return (
     <div className="github-container">
@@ -49,22 +52,61 @@ function Github() {
 
       <div className="ul-flexbox">
         {" "}
-        <div className="repo-card">
-          <ul className="list-container">
-            {repos &&
-              repos.map((repo) => (
-                <li className="links" key={repo.id}>
-                  <a
-                    className="link-style"
-                    href={repo.html_url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {repo.name}
-                  </a>{" "}
-                </li>
-              ))}
-          </ul>
+        <div className="repo-cards">
+          {filteredRepos.map((repo, index) => (
+            <div className="repo-card" key={repo.id}>
+              <a
+                className="link-style"
+                href={repo.html_url}
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ width: "60%" }}
+              >
+                <h3 className="repo-card-title">{repo.name}</h3>
+
+                {index === 0 && ( // Villkorsstyrd rendering för att visa ikonen bara på det första kortet
+                  <span className="icons">
+                    <FontAwesomeIcon
+                      className="javascript-icon"
+                      icon={faJs}
+                      size="4x"
+                    />
+
+                    <FontAwesomeIcon
+                      className="css-icon"
+                      icon={faCss3Alt}
+                      size="4x"
+                    />
+                    <FontAwesomeIcon
+                      className="html-icon"
+                      icon={faHtml5}
+                      size="4x"
+                    />
+                  </span>
+                )}
+                {index === 1 && ( // Villkorsstyrd rendering för att visa ikonen bara på det första kortet
+                  <span className="icons-second">
+                    <FontAwesomeIcon
+                      className="javascript-icon"
+                      icon={faReact}
+                      size="4x"
+                    />
+
+                    <FontAwesomeIcon
+                      className="css-icon"
+                      icon={faCss3Alt}
+                      size="4x"
+                    />
+                    <FontAwesomeIcon
+                      className="html-icon"
+                      icon={faHtml5}
+                      size="4x"
+                    />
+                  </span>
+                )}
+              </a>
+            </div>
+          ))}
         </div>
       </div>
     </div>
